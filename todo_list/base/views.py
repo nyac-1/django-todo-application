27eeb2b0330ca335380758bc2f35719fc4d257ee
nyac-1 <routusampath@yahoo.com>
2021-12-26 20:40:15 +0530

    update: added base list integration

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def tasklist(request):
-    return HttpResponse('To Do List')
+from django.views.generic.list import ListView
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
 
