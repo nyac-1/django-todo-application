8cf281a30e3a767afa68bdd000b222c464554520
nyac-1 <routusampath@yahoo.com>
2021-12-26 21:32:52 +0530

    update: basic individual card backend for a task

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 class TaskList(ListView):
@@ -9,4 +10,8 @@ class TaskList(ListView):
     context_object_name = 'tasks'
 
 
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
 
