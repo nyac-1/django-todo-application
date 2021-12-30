4ef29995d926f496498fa2504dc65ecc6cf22ac9
nyac-1 <routusampath@yahoo.com>
2021-12-30 13:31:37 +0530

    update: user details and restricting pages

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -112,11 +112,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
